49524df31fb857e3b9b54074fd04fcfabde52fa1
Diana Berezan <berezandiana24@gmail.com>
2025-09-26 20:35:46 +0300

    fix

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,18 @@
 import { MovieData } from './types/MovieData';
-import { ResponseError } from './types/ReponseError';
+import { ResponseError } from './types/ResponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=d1257887';
+const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${encodeURIComponent(query)}`)
-    .then(res => res.json())
+    .then(res => {
+      if (!res.ok) {
+        return { Response: 'False', Error: `HTTP error: ${res.status}` };
+      }
+
+      return res.json();
+    })
     .catch(() => ({
       Response: 'False',
       Error: 'unexpected error',
